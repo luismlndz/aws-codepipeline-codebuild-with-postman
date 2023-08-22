98f01d8368434bf70f0e94ed6e4c25b1626d81e9
Luis Melendez <luis@space-eyes.com>
2023-08-22 18:54:19 -0400

    renamed and edited files

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -6,16 +6,16 @@
 echo "Running update-postman-env-file.sh"
 
 api_gateway_url=`aws cloudformation describe-stacks \
-  --stack-name petstore-api-stack \
+  --stack-name space-eyes-api-stack \
   --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
 
 echo "API Gateway URL:" ${api_gateway_url}
 
 jq -e --arg apigwurl "$api_gateway_url" '(.values[] | select(.key=="apigw-root") | .value) = $apigwurl' \
-  PetStoreAPIEnvironment.postman_environment.json > PetStoreAPIEnvironment.postman_environment.json.tmp \
-  && cp PetStoreAPIEnvironment.postman_environment.json.tmp PetStoreAPIEnvironment.postman_environment.json \
-  && rm PetStoreAPIEnvironment.postman_environment.json.tmp
+  SpaceEyesAPIEnvironment.postman_environment.json > SpaceEyesAPIEnvironment.postman_environment.json.tmp \
+  && cp SpaceEyesAPIEnvironment.postman_environment.json.tmp SpaceEyesAPIEnvironment.postman_environment.json \
+  && rm SpaceEyesAPIEnvironment.postman_environment.json.tmp
 
-echo "Updated PetStoreAPIEnvironment.postman_environment.json"
+echo "Updated SpaceEyesAPIEnvironment.postman_environment.json"
 
-cat PetStoreAPIEnvironment.postman_environment.json
+cat SpaceEyesAPIEnvironment.postman_environment.json
